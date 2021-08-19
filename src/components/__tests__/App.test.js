423a5cf8268929955fac33f53279d105353d9516
seanfinnessy <seanfinnessy@icloud.com>
2021-08-19 16:17:13 -0400

    added absolute paths

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { shallow } from "enzyme";
-import App from "../App";
-import CommentBox from '../CommentBox';
-import CommentList from "../CommentList";
+
+import App from "components/App";
+import CommentBox from "components/CommentBox";
+import CommentList from "components/CommentList";
+
+// runs before below tests. can initialize variables easily. pay attention to function scopes. must reassign values to wrapped, so use let.
+let wrapped;
+beforeEach(() => {
+  wrapped = shallow(<App />);
+});
 
 it("shows a comment box", () => {
-  const wrapped = shallow(<App />);
   expect(wrapped.find(CommentBox).length).toEqual(1);
 });
 
 it("shows a comment list", () => {
-  const wrapped = shallow(<App />);
   expect(wrapped.find(CommentList).length).toEqual(1);
-})
+});
